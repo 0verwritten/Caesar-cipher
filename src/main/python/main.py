370765e20b86cb7fa90ccaf39f8a0d50d3322410
TheSimpleWriter <simplepcwriter@gmail.com>
2019-06-19 09:05:08 +0300

    first app version

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -10,14 +10,20 @@ class MainApp(QtWidgets.QApplication, Ui_MainWindow):
         super(MainApp, self).__init__([])
         self.mainwindow = QtWidgets.QMainWindow()
         self.setupUi(self.mainwindow)
-        self.lineEdit
+        self.lineEdit.setValidator(QtGui.QIntValidator())
         self.pushButton_2.pressed.connect(self.add)
         self.pushButton.pressed.connect(self.remove)
+        self.textBrowser.textChanged.connect(self.encrypt)
         self.mainwindow.show()
     def add(self):
         self.lineEdit.setText(str(int(self.lineEdit.text())+1))
     def remove(self):
         self.lineEdit.setText(str(int(self.lineEdit.text())-1))
+    def encrypt(self):
+        shift = int(self.lineEdit.text())
+        text = self.textBrowser.toPlainText()
+        ascii_from_text = [ord(c) for c in text]
+        self.textBrowser_2.setText(''.join(chr(i+shift) for i in ascii_from_text))
 
 
 if __name__ == '__main__':
